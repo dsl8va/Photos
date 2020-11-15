db3389e03dd979e3d2299329fa6fd4cd66783b66
David Kim <davidhk21@gmail.com>
2020-11-15 00:44:43 -0700

    updated tests and fixing err 404 test

--- server/app.js
+++ server/app.js
@@ -3,14 +3,19 @@ const app = express();
 const db = require('../db/index.js');
 const path = require('path');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/api/listings/:id', (req, res) => {
-  db.Listings.findOne({
-    id: req.params.id
-  })
+  // const listing = await db.Listings.findOne({id: req.params.id});
+  // if (!listing) {
+  //   res.status(404).send('Listing does not exist')
+  // } else {
+  //   res.status(200).send(listing);
+  // }
+
+  db.Listings.findOne({id: req.params.id})
     .then(listing => {
       res.status(200).send(listing);
     })
--- test/server.test.js
+++ test/server.test.js
@@ -6,27 +6,21 @@ const supertest = require('supertest');
 // allows to make API calls on routes
 const request = supertest(app);
 
-beforeAll(async (done) => {
-  await mongoose.disconnect();
+describe('Test /api/listings/:id path', () => {
 
-  mongoose.connect('mongodb://localhost/photosTest', { useNewUrlParser: true, useUnifiedTopology: true });
-  const db = mongoose.connection;
-  db.on('error', (err) => done.fail(err));
-  db.once('open', () => done());
-  done();
-});
-
-// Runs after all tests in this file have finished!
-
-describe('Test /api/listings path', () => {
-  it('should respond to GET method', async (done) => {
-    const response = await request.get('/api/listings/1');
-    expect(response.statusCode).toBe(200);
+  it('should respond to GET method with each listing by its id', async (done) => {
+    for (var id = 1; id <= 100; id++) {
+      let res = await request.get(`/api/listings/${id}`);
+      expect(res.statusCode).toBe(200);
+      expect(res.body.id).toEqual(id);
+    }
     done();
-  });j
-});
+  });
 
-afterAll(async (done) => {
-  await mongoose.disconnect();
-  done();
+  it('should respond with 404 for a listing that does not exist', async (done) => {
+    let res = await request.get('/api/listings/101');
+    expect(res.statusCode).toBe(404);
+    done();
+  })
+
 });
