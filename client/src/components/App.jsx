a21268caa6a17f2abd1fc0f115ecc5a2b0cea72f
David Kim <davidhk21@gmail.com>
2020-11-22 23:49:29 -0700

    changed selectedImg state to be handled from App.jsx to handle 'escape' functionality property

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import {FiHeart, FiShare} from 'react-icons/fi'
 import {FaRegImage} from 'react-icons/fa'
 import FirstModal from './Modal1.jsx';
+import ImageCarousel from './Modal2.jsx';
 import * as Styled from '../styles/styles.js';
 import s from '../styles/styles.css';
 
@@ -17,6 +18,7 @@ const Image3 = styled.div`${Styled.Image3Style}`;
 const App = () => {
   const [listing, setListing] = useState({});
   const [modal1IsOpen, setModal1IsOpen] = useState(false);
+  const [selectedImg, setSelectedImg] = useState(null);
 
   useEffect(() => {
     fetchListing()
@@ -35,8 +37,9 @@ const App = () => {
   const keyPress = useCallback(e => {
     if (e.key === 'Escape' && modal1IsOpen) {
       setModal1IsOpen(false);
+      setSelectedImg(null);
     }
-  }, [setModal1IsOpen, modal1IsOpen]);
+  }, [setModal1IsOpen, modal1IsOpen, setSelectedImg]);
 
   useEffect(() => {
     document.addEventListener('keydown', keyPress);
@@ -66,7 +69,10 @@ const App = () => {
 
     </Styled.Container>
 
-    <FirstModal open={modal1IsOpen} onClose={() => setModal1IsOpen(false)} listing={listing}/>
+    <FirstModal open={modal1IsOpen} onClose={() => setModal1IsOpen(false)} listing={listing} setSelectedImg={setSelectedImg}/>
+
+    <ImageCarousel selectedImg={selectedImg} onClose={() => setSelectedImg(false)} listing={listing} setSelectedImg={setSelectedImg}/>
+
     </>
   )
 }
